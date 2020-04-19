46e83c5bc2c3add39d66e29b58c4a6696a65ca39
Chef612 <1806078@kiit.ac.in>
2020-04-19 19:05:06 +0530

    f

---
+++ login.js
@@ -0,0 +1,21 @@
+var attempt = 3;
+function validate(){
+var username = document.getElementById("username").value;
+var password = document.getElementById("password").value;
+if ( username == "Souvik" && password == "doc1"){
+alert ("Login successfully");
+window.location = "http://localhost/phpmyadmin/sql.php?db=tutorial&table=person&pos=0"; // Redirecting to other page.
+return false;
+}
+else{
+attempt --;// Decrementing by one.
+alert("You have left "+attempt+" attempt;");
+// Disabling fields after 3 attempts.
+if( attempt == 0){
+document.getElementById("username").disabled = true;
+document.getElementById("password").disabled = true;
+document.getElementById("submit").disabled = true;
+return false;
+}
+}
+}
